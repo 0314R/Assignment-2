9945c04de57d8575c1d5232e0ebf5834823163aa
a2504 <59727073+a2504@users.noreply.github.com>
2022-03-08 14:49:50 +0100

    Update pageclock.c

--- pageclock.c
+++ pageclock.c
@@ -2,35 +2,63 @@
 #include <stdlib.h>
 #include <math.h>
 
+int find(int *arr, int len, int page)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] == page)
+            return i;
+    }
+    return -1;
+}
+
 int main(int argc, char *argv[])
 {
     int frames;
     scanf("%d", &frames);
 
     int *arr = (int *)malloc(frames * sizeof(int));
+    int *rbit = (int *)malloc(frames * sizeof(int));
+
+    // char x = getchar(); // gets spaces and newlines
+
     for (int i = 0; i < frames; i++)
     {
         int x;
         scanf("%d", &x);
         arr[i] = x;
+        rbit[i] = 0;
     }
     int misses = frames;
 
     int page, idx = 0;
     char c;
+
     while ((c = getchar()) != EOF && c != '\n')
     {
         scanf("%d", &page);
-        if (arr[idx] != page)
+        int findIdx = find(arr, frames, page);
+
+        if (findIdx == -1) // page fault
         {
             misses++;
+            while (rbit[idx] == 1)
+            {
+                rbit[idx] = 0;
+                idx = (idx + 1) % frames;
+            }
             arr[idx] = page;
+            idx = (idx + 1) % frames;
         }
-        idx = (idx + 1) % frames;
+        else
+        {
+            rbit[findIdx] = 1;
+        }
     }
 
     printf("%d\n", misses);
 
+    free(rbit);
     free(arr);
 
     return 0;
