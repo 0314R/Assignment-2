f9edcc22c72a10bc8481cd3694b96132e2b4a86d
a2504 <59727073+a2504@users.noreply.github.com>
2022-03-08 17:42:06 +0100

    update

--- pageclock.c
+++ pageclock.c
@@ -17,27 +17,32 @@ int main(int argc, char *argv[])
     int frames;
     scanf("%d", &frames);
 
+    // arrays for the frames and the R bit
     int *arr = (int *)malloc(frames * sizeof(int));
     int *rbit = (int *)malloc(frames * sizeof(int));
 
+    // set both to zero
     for (int i = 0; i < frames; i++)
     {
         arr[i] = 0;
         rbit[i] = 0;
     }
-    int misses = 0;
 
-    int page, idx = 0;
+    int page, idx = 0, misses = 0;
     char c;
+    // flag makes sure the loop doesn't end immediately, since the first char read is a newline
     int flag = 0;
 
     while (((c = getchar()) != EOF && c != '\n') || (flag == 0))
     {
         flag = 1;
+        // scan new page and check if it is already in the array
         scanf("%d", &page);
         int findIdx = find(arr, frames, page);
 
-        if (findIdx == -1) // page fault
+        // if papge fault: increase misses, replace the first page that has R bit = 0
+        // and update the idx accordingly
+        if (findIdx == -1)
         {
             misses++;
             while (rbit[idx] == 1)
@@ -48,6 +53,7 @@ int main(int argc, char *argv[])
             arr[idx] = page;
             idx = (idx + 1) % frames;
         }
+        // if no page fault, just set R bit to 1
         else
         {
             rbit[findIdx] = 1;
--- pagefifo.c
+++ pagefifo.c
@@ -24,19 +24,21 @@ int main(int argc, char *argv[])
     {
         arr[i] = 0;
     }
-    int misses = 0;
 
     // page reads the input, idx keeps track of which frame should be compared/updated
-    // time keeps track of the number of updates over all frames
-    int page, idx = 0, time = frames;
+    // time keeps track of the number of updates over all frames, misses = #page faults
+    int page, idx = 0, time = frames, misses = 0;
     // c makes sure the newline is read at the end
     char c;
+    // flag makes sure the loop doesn't end immediately, since the first char read is a newline
     int flag = 0;
 
     while (((c = getchar()) != EOF && c != '\n') || (flag == 0))
     {
         flag = 1;
+        // scan new page
         scanf("%d", &page);
+        // if page fault: increase misses, replace page and update index
         if (find(arr, frames, page) == -1)
         {
             misses++;
